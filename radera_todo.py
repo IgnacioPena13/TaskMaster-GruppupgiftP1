2c298c887bcecf4cbb7ca13eaaa1d3930fdb4f3a
Ignacio <ignacio.penasepulveda@outlook.com>
2026-09-25 14:12:35 +0200

    Implement radera functionality and update menu for task deletion

--- radera_todo.py
+++ radera_todo.py
@@ -3,13 +3,17 @@ import json
 
 def radera():
     with open("dummy.txt","r", encoding="utf-8") as f:
-        data = json.load(f)
-        print(data)
+        data = json.load(f) #ska vara en lista med dictionaries
+        for j in data:
+            print(f'{j}', end='\n')
         print('id på uppgiften du vill ta bort?')
         id_radera = int(input('> '))
         print("uppgiften raderas...")
-        for i in data:
+        for i in data: #checkar om det finns en upppgiftmed id man har skrivit
             if i["id"] == id_radera:
                 data.pop(id_radera-1)
-        for j in data:
-            print(f'Uppgift: {j["uppgift"]}:\nPrioritet: {j["prioritet"]}')
+        for j in data: #printas igen data utan den uppgift man ville ta bort
+            print(f'{j}', end='\n')
+        print('VILL DU SPARA DE HÄR UPPGIFTER? J/N')
+        spara = input('> ')
+        #koden/funktionen till filhantering
