4c6409062ce47ea25970a80942ccda076c12413a
DAEMON <daemonoberg@gmail.com>
2026-09-25 14:57:44 +0200

    Fixed the code

--- Filhantering.py
+++ Filhantering.py
@@ -16,3 +16,21 @@ def spara_till_fil(uppgifter):
 
 
 def läsa_från_fil():
+    try:
+        uppgifter = []
+        filnamn = "uppgifter.txt"
+        fil = open(filnamn, "r")
+        rader = fil.readlines()
+        
+        
+        for rad in rader:
+            delar = rad.split("|")
+            ny_uppgift = {"id": int(delar[0]), "uppgift": (delar[1]), "prioritet": (delar[2].strip())}
+            uppgifter.append(ny_uppgift)
+        fil.close()
+        return uppgifter
+    except FileNotFoundError:
+        return uppgifter
+
+
+
