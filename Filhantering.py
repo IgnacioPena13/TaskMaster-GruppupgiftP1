81877bd537b692abd4430ab6665814a40043216b
DAEMON <daemonoberg@gmail.com>
2026-09-25 15:04:26 +0200

    improved readability byte plats på läsa och spara

--- Filhantering.py
+++ Filhantering.py
@@ -1,20 +1,4 @@
 
-
-def spara_till_fil(uppgifter):
-    filnamn = "uppgifter.txt"
-    fil = open(filnamn, "w")
-    
-    
-    for uppgift in uppgifter:
-        fil.write(str(uppgift["id"]))
-        fil.write("|")
-        fil.write(uppgift["uppgift"])
-        fil.write("|")
-        fil.write(uppgift["prioritet"])
-        fil.write("\n")
-    fil.close()
-
-
 def läsa_från_fil():
     try:
         uppgifter = []
@@ -33,4 +17,16 @@ def läsa_från_fil():
         return uppgifter
 
 
-
+def spara_till_fil(uppgifter):
+    filnamn = "uppgifter.txt"
+    fil = open(filnamn, "w")
+    
+    
+    for uppgift in uppgifter:
+        fil.write(str(uppgift["id"]))
+        fil.write("|")
+        fil.write(uppgift["uppgift"])
+        fil.write("|")
+        fil.write(uppgift["prioritet"])
+        fil.write("\n")
+    fil.close()
